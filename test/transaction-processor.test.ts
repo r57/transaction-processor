e8ef2682230c1956dd687ea479266d595f45c36e
Petr Slajchrt <petr@slajchrt.cz>
2023-04-02 10:56:25 +0200

    Initial commit

---
+++ test/transaction-processor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TransactionProcessor from '../lib/transaction-processor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/transaction-processor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TransactionProcessor.TransactionProcessorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
